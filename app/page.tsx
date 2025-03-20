1eec73ca14db74f815d895a1ccef857d29232187
Leandro Rodrigues <lj251004@gmail.com>
2025-03-20 14:05:46 -0300

    fix: remove favicon and update global styles; change layout and homepage content for personal portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <main className="max-w-4xl mx-auto p-4 md:p-8">
+      <h1 className="font-medium">Leandro Bertalhia</h1>
+    </main>
+  );
 }
